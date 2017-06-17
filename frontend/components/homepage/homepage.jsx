951f2d5da71791d64ba9f8c282eb12576ab700ef
Winber Xu <winber1@gmail.com>
2017-06-17 00:01:29 -0700

    viewport tests

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -11,12 +11,13 @@ class Homepage extends React.Component {
         <div className="parallax">
           <div className="parralax__group" id="group-1">
             <div className="parallax__layer" id="base">
-              <h1>Winber Xu</h1>
-              <h1>Software Engineer</h1>
             </div>
           </div>
           <div className="parralax__group" id="group-2">
-            <div className="parallax__layer" id="index"></div>
+            <div className="parallax__layer" id="index">
+              <h1>Winber Xu</h1>
+              <h1>Software Engineer</h1>
+            </div>
           </div>
         </div>
       </div>
