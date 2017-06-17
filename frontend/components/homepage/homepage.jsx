4acb39453a933e56f922f37c89635a0a20b908af
Winber Xu <winber1@gmail.com>
2017-06-17 01:33:59 -0700

    arrow

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -14,9 +14,14 @@ class Homepage extends React.Component {
             </div>
           </div>
           <div className="parralax__group" id="group-2">
-            <div className="parallax__layer" id="index">
+            <div className="parallax__layer" id="name">
               <h1>Winber Xu</h1>
               <h1>Software Engineer</h1>
+            </div>
+            <div className="parallax__layer" id="index">
+              <div className="arrow-box">
+                <div className="arrow"></div>
+              </div>
             </div>
           </div>
         </div>
