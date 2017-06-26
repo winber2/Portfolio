3c57b7fcd97dbd7a521be3c344b36f847eff608c
Winber Xu <winber1@gmail.com>
2017-06-25 18:50:47 -0700

    refactor about me

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Title from './title';
+import About from './about';
 
 class Homepage extends React.Component {
   constructor(props) {
@@ -61,25 +62,7 @@ class Homepage extends React.Component {
 
         <main>
           <Title />
-          <div className="about">
-            <h1>about</h1>
-            <section className="about-and-skills">
-              <aside className="about">
-                <h2>me</h2>
-                <p>I'm a software engineer based in San Francisco focused on giving the best user experience possible, and I'm looking for some cool projects to work on. Let's tackle some challenges together.</p>
-
-                <p>Technology is awesome, and I want to explore what it can do. Why don't you check out some of my projects.</p>
-              </aside>
-              <ul className="skills">
-                <h2>skills</h2>
-                <li>HTML</li>
-                <li>HTML</li>
-                <li>HTML</li>
-                <li>HTML</li>
-                <li>HTML</li>
-              </ul>
-            </section>
-          </div>
+          <About />
           <div className="border" />
           <div className="projects">
             <article className="project"></article>
