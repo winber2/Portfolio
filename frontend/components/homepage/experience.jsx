0f45bf8d76adc4a86b483768724ae6419267ca97
Winber Xu <winber1@gmail.com>
2017-07-19 22:58:58 -0700

    remove useless script tags

---
+++ frontend/components/homepage/experience.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Gadgets extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="experience">
+        <aside className="timeline">
+
+        </aside>
+        <section className="work-history">
+
+        </section>
+      </div>
+    )
+  }
+}
---
+++ frontend/components/homepage/gadgets.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Gadgets extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <li className="gadgets">
+        <aside className="gadgets-description">
+
+        </aside>
+        <
+      </li>
+    )
+  }
+}
