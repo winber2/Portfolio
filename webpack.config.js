707a9c70e648e29494c29ba436d2e666a55cdd0a
Winber Xu <winber1@gmail.com>
2017-07-21 01:27:10 -0700

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[name][md5:hash].[ext]', // Name of bundled asset
-            outputPath: '/webpack-assets', // Output location for assets. Final: `app/assets/webpack/webpack-assets/`
+            outputPath: '/webpack-assets/', // Output location for assets. Final: `app/assets/webpack/webpack-assets/`
             publicPath: '/assets' // Endpoint asset can be found at on Rails server
           }
         }
